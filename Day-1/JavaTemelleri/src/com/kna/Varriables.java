95fd55074841e1d41ab23d0767d4de78c9fbc0c6
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-07-21 12:10:05 +0300

    Create to Day-1 file

---
+++ Day-1/JavaTemelleri/src/com/kna/Varriables.java
@@ -0,0 +1,70 @@
+package com.kna;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Varriables {
+
+    public static void main(String[] args) {
+        //Varriables
+
+        //Integer - Long
+
+        int age = 20;
+
+        System.out.println(10*age);
+        System.out.println(age/5);
+
+        int x = 5;
+        int y = 11;
+
+        System.out.println(y/x);
+
+        long myLong = 10;
+        System.out.println(myLong / 5);
+
+        //Double - Float
+        double z = 5.0;
+        double a = 11.0;
+
+        System.out.println(a/z);
+
+        float myFloat = 10.0f;
+
+        double pi = 3.14;
+
+        int r = 5;
+
+        System.out.println(2*pi*r);
+
+        //String
+
+        String name = "Faik";
+        String surname = "Turan";
+
+        String fullName = name + " " + surname;
+        System.out.println(fullName);
+
+        //Boolean
+
+        boolean isAlive = true;
+        isAlive = false;
+        System.out.println(isAlive);
+
+        //Final
+
+        final int myInteger = 5;
+
+        System.out.println("myInteger " + myInteger);
+
+        //myInteger = 4;
+
+        System.out.println("myInteger " + myInteger);
+
+
+
+
+
+
+    }
+
+}
