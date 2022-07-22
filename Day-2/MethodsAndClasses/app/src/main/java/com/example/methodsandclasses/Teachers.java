5b6795c8bdf74b1c3e324ee19bf72b03ba1161f9
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-07-22 09:23:01 +0300

    Add to java file

---
+++ Day-2/MethodsAndClasses/app/src/main/java/com/example/methodsandclasses/Teachers.java
@@ -0,0 +1,24 @@
+package com.example.methodsandclasses;
+
+public class Teachers {
+    //attribute
+
+    String name;
+    String branch;
+    int age;
+
+    public Teachers(String name, String branch, int age) {
+        this.name = name;
+        this.branch = branch;
+        this.age = age;
+    }
+
+    public Teachers(String name, String branch) {
+        this.name = name;
+        this.branch = branch;
+    }
+
+    public Teachers() {
+
+    }
+}
