2ba679f945bba9dbfbbd537ff23cb2aea796104a
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-07-30 12:28:57 +0300

    Add to Day-8 files

---
+++ Day-8/CarCollection/app/src/main/java/com/example/carcollection/Car.java
@@ -0,0 +1,11 @@
+package com.example.carcollection;
+
+public class Car {
+    String name;
+    int id;
+
+    public Car(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
