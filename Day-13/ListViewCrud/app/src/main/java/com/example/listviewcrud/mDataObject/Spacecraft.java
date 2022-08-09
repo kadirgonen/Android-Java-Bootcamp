21d46beb4e13c063b6c7f2ecb87e1531275cb845
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-08-09 10:40:21 +0300

    Add to Day-13 files

---
+++ Day-13/ListViewCrud/app/src/main/java/com/example/listviewcrud/mDataObject/Spacecraft.java
@@ -0,0 +1,25 @@
+package com.example.listviewcrud.mDataObject;
+
+public class Spacecraft {
+    String name;
+    int id;
+
+    public Spacecraft() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
